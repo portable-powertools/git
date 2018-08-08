b9b3ba9892b2367141a7e295abf7c95248cc0c72
Simon Leischnig <simon-jena@gmx.net>
2018-08-08 12:50:45 +0200

    fixed automatic symlinking

--- interactive.sh
+++ interactive.sh
@@ -3,4 +3,4 @@ if [[ -e "$HOME/.gitconfig" && ! -L "$HOME/.gitconfig" ]]; then
     mv "$HOME/.gitconfig" "$HOME/.gitconfig.old"
 fi
 ln -sf "$mod_git_config/global.gitconfig" "$HOME/.gitconfig"
-ln -sf "$mod_git_config" "$HOME/.config/module_git" 
+ln -sfT "$mod_git_config" "$HOME/.config/module_git" 
